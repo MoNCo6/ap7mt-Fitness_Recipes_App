f77befabaeca5a392b3f94b51efbf89389408308
Martin Blaho <blaho.martin6@gmail.com>
2025-01-28 23:03:22 +0100

    Pridanie SplashActivity do priečinka View bez úpravy kódu.

--- app/src/main/java/com/example/finalproject_fitrecipesapp/view/SplashActivity.kt
+++ app/src/main/java/com/example/finalproject_fitrecipesapp/view/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.example.finalproject_fitrecipesapp
+package com.example.finalproject_fitrecipesapp.view
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import com.example.finalproject_fitrecipesapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
